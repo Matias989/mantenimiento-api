170092d022696f1697b35f63a00c16b393131547
matias <matiaspena93@hotmail.com>
2023-04-04 12:27:13 -0300

    Cambio por tarea

--- mantenimiento-api/Program.cs
+++ mantenimiento-api/Program.cs
@@ -77,6 +77,8 @@ builder.Services.AddAutoMapper(typeof(UserProfile));
 
 var app = builder.Build();
 
+//Cambio por tarea
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
